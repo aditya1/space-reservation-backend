7d0eb42b5563d8822e37ef5826fa90ec4fa5346a
Aditya Agashe <aditya87@vbi.vt.edu>
2022-01-29 14:38:58 -0500

    Enabling esbuild

--- cdk/SpaceStack.ts
+++ cdk/SpaceStack.ts
@@ -8,6 +8,7 @@ import {
 import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway'
 import { join } from 'path'
 import { GenericTable } from './GenericTable'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 export class SpaceStack extends Stack {
 	private api = new RestApi(this, 'SpaceApi')
 	private spacesTable = new GenericTable('SpacesTable', 'spaceId', this)
@@ -18,6 +19,11 @@ export class SpaceStack extends Stack {
 			runtime: Runtime.NODEJS_14_X,
 			code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
 			handler: 'hello.main',
+		})
+
+		const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+			entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
+			handler: 'handler',
 		})
 
 		// Hello Api lambda integration:
